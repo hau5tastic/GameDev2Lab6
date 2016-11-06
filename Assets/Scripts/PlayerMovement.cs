f7d62eb616a93aaf5c1b1a43e5e524e2314fabc0
hau5tastic <hau5tastic@gmail.com>
2016-11-06 11:41:51 -0500

    Nicos Stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour {
+
+    private Transform charTransform;
+    private float moveSpeed = 10.0f;
+
+	void Start () {
+        charTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateMovement();
+	}
+
+    private void UpdateMovement() {
+        if (!isLocalPlayer) return;
+
+        float xMove = Input.GetAxis("Vertical");
+        float yMove = Input.GetAxis("Horizontal");
+
+        charTransform.position += charTransform.forward * xMove * moveSpeed * Time.deltaTime;
+        charTransform.position += charTransform.right * yMove * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerView.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerView : NetworkBehaviour {
+
+
+    private float XSensitivity = 2;
+    private float YSensitivity = 2;
+
+    private Transform charTransform;
+    private Transform charCamera;
+
+    void Start () {
+        if (!isLocalPlayer)
+            GetComponentInChildren<Camera>().enabled = false;
+
+        charTransform = GetComponent<Transform>();
+        charCamera = GetComponentInChildren<Camera>().transform;
+
+
+        Cursor.lockState = CursorLockMode.None;
+        // Cursor.visible = false;
+    }
+	
+	void Update () {
+        UpdateView();
+    }
+
+    private void UpdateView() {
+        if (!isLocalPlayer) return;
+
+        float xRot = Input.GetAxis("Mouse X") * XSensitivity;
+        float yRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
+        charCamera.localRotation *= Quaternion.Euler(-yRot, 0, 0f);
+        charTransform.localRotation *= Quaternion.Euler(0, xRot, 0f);
+
+        Debug.DrawRay(transform.position, transform.forward.normalized * 5.0f, Color.green);
+    }
+}
